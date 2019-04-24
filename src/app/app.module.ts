430f17938e74b44d3c8533d25c076bc158688183
Jefer Bustamante <jeferb93@gmail.com>
2019-04-23 21:45:59 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
+import { Ajuste2Page, AjustesPage, ModalPage, PrincipalPage, Pagina2Page, Pagina3Page, TabsPage } from '../pages/index.pages';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    Ajuste2Page,
+    AjustesPage,
+    ModalPage,
+    PrincipalPage,
+    Pagina2Page,
+    Pagina3Page,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    Ajuste2Page,
+    AjustesPage,
+    ModalPage,
+    PrincipalPage,
+    Pagina2Page,
+    Pagina3Page,
+    TabsPage
   ],
   providers: [
     StatusBar,
